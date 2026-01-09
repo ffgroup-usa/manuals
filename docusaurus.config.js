15a128aa0db9dc5c501aed0ca8cef10b8d58b060
Alex Andreyev <a.andreyev@ff-group.org>
2026-01-08 16:46:46 -0800

    Add GitHub Pages deployment workflow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,17 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ffgroup-usa.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/manuals/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ffgroup-usa',
+  projectName: 'manuals',
+  trailingSlash: false,
+  onBrokenMarkdownLinks: 'warn',
 
   onBrokenLinks: 'throw',
 
